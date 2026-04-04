fc5904bce94e6fe36e3ee828de514d0ecb84deda
Jonk25 <jonk1983@mail.ru>
2026-04-04 23:53:32 +0300

    1

---
+++ smart-table/tests-smart-table/playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  reporter: 'list',
+  timeout: 7 * 1000,
+  workers: '50%',
+  testDir: './tests',
+  outputDir: './tmp/artifacts',
+  use: {
+    baseURL: 'http://localhost:5173',
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'only-on-failure',
+    locale: 'ru-RU',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    cwd: process.env.GITHUB_WORKSPACE,
+    command: 'npm run dev',
+    port: 5173,
+    reuseExistingServer: true
+  }
+});
